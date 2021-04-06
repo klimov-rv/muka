111bd9b25d3ace11e7943f36e2f5c4e1e2954ac4
klimov-rv <klimov.rv@yandex.ru>
2021-04-06 20:29:21 +0300

    zakupka add

--- assets/common.js
+++ assets/common.js
@@ -39,7 +39,6 @@ $(".js-hamburger").click(function () {
 
 
 document.getElementById("defaultOpen").click();
-document.getElementById("defaultOpen2").click();
 
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
@@ -59,27 +58,3 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
-function openMegaTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-
-  tabcontent = document.getElementsByClassName("megatabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("megatablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-    tabSlider0.update();
-    tabSlider1.update();
-  }
-
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  window.addEventListener("load", scroll);
-  window.addEventListener("scroll", scroll);
-});
