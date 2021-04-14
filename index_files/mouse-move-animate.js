2cc27a3468bc540ab223e31180deb9987a7ecb07
klimov-rv <klimov.rv@yandex.ru>
2021-04-15 02:15:19 +0300

    super stucture

--- index_files/mouse-move-animate.js
+++ index_files/mouse-move-animate.js
@@ -1,19 +1,19 @@
 
 if ($('.js-parallax__first-layer').length > 0) { 
     $(function () {
-        $('.js-parallax__first-layer').plaxmove({ ratioH: 0.31, ratioV: 0 })
+        $('.js-parallax__first-layer').plaxmove({ ratioH: 0.27, ratioV: 0 })
     });
 }
 if ($('.js-parallax__second-layer').length > 0) { 
     $(function () {
-        $('.js-parallax__second-layer').plaxmove({ ratioH: 0.19, ratioV: 0 })
+        $('.js-parallax__second-layer').plaxmove({ ratioH: 0.15, ratioV: 0 })
     }); 
 }
-
-if ($('.animate1_2').length > 0) { 
+ 
+if ($('.zakupka_animate').length > 0) { 
     $(function () {
-        $('.js-parallax__first-layer').plaxmove({ ratioH: 0.05, ratioV: 0 })
-        $('.js-parallax__second-layer').plaxmove({ ratioH: 0.01, ratioV: 0 })
+        $('.zakupka_animate1').plaxmove({ ratioH: 0.05, ratioV: 0 })
+        $('.zakupka_animate2').plaxmove({ ratioH: 0.01, ratioV: 0 })
     }); 
 }
 
