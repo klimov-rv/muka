d58b776d39185b4ef73a6d7717cf6e9e9adf82e0
klimov-rv <klimov.rv@yandex.ru>
2021-04-16 12:35:08 +0300

    update 2 (beta)

--- assets/js/mouse-move-animate.js
+++ assets/js/mouse-move-animate.js
@@ -1,96 +1,96 @@
 
-if ($('.js-parallax__first-layer').length > 0) { 
-    $(function () {
-        $('.js-parallax__first-layer').plaxmove({ ratioH: 0.27, ratioV: 0 })
-    });
+if ($('.js-parallax__first-layer').length > 0) {
+	$(function () {
+		$('.js-parallax__first-layer').plaxmove({ ratioH: 0.055, ratioV: 0.035 })
+	});
 }
-if ($('.js-parallax__second-layer').length > 0) { 
-    $(function () {
-        $('.js-parallax__second-layer').plaxmove({ ratioH: 0.15, ratioV: 0 })
-    }); 
+if ($('.js-parallax__second-layer').length > 0) {
+	$(function () {
+		$('.js-parallax__second-layer').plaxmove({ ratioH: 0.01, ratioV: 0.015 })
+	});
 }
- 
-if ($('.zakupka_animate').length > 0) { 
-    $(function () {
-        $('.zakupka_animate1').plaxmove({ ratioH: 0.05, ratioV: 0 })
-        $('.zakupka_animate2').plaxmove({ ratioH: 0.01, ratioV: 0 })
-    }); 
+
+if ($('.zakupka_animate').length > 0) {
+	$(function () {
+		$('.zakupka_animate1').plaxmove({ ratioH: 0.01, ratioV: 0.015 })
+		$('.zakupka_animate2').plaxmove({ ratioH: 0.009, ratioV: 0.009 })
+	});
 }
 
 (function ($) {
-    $.fn.plaxmove = function (options) {
+	$.fn.plaxmove = function (options) {
 
-        this.defaults = {
-            ratioH: 0,
-            ratioV: 1,
-            invertH: false,
-            invertV: false,
-            reversed: false
-        }
+		this.defaults = {
+			ratioH: 0.01,
+			ratioV: 0.03,
+			invertH: false,
+			invertV: false,
+			reversed: false
+		}
 
-        var settings = $.extend({}, this.defaults, options),
-            layer = $(this),
-            center = {
-                x: $('html').width() / 2 - layer.width() / 2,
-                y: $('html').height() / 2 - layer.height() / 2
-            },
-            y0 = layer.offset().top,
-            x0 = 0;
+		var settings = $.extend({}, this.defaults, options),
+			layer = $(this),
+			center = {
+				x: $('html').width() / 100 - layer.width() / 1,
+				y: $('html').height() / 100 - layer.height() / 1
+			},
+			y0 = layer.position().top,
+			x0 = layer.position().left;
 
-        if (settings.reversed) {
+		if (settings.reversed) {
 
-            if (settings.invertH)
-                var eqH = function (e) {
-                    return x0 - (e.pageY - center.y) * settings.ratioH
-                }
+			if (settings.invertH)
+				var eqH = function (e) {
+					return x0 - (e.pageY - center.y) * settings.ratioH
+				}
 
-            else
-                var eqH = function (e) {
-                    return x0 + (e.pageY - center.y) * settings.ratioH
-                }
+			else
+				var eqH = function (e) {
+					return x0 + (e.pageY - center.y) * settings.ratioH
+				}
 
-            if (settings.invertV)
-                var eqW = function (e) {
-                    return y0 - (e.pageX - center.x) * settings.ratioV
-                }
-            else
-                var eqW = function (e) {
-                    return y0 + (e.pageX - center.x) * settings.ratioV
-                }
+			if (settings.invertV)
+				var eqW = function (e) {
+					return y0 - (e.pageX - center.x) * settings.ratioV
+				}
+			else
+				var eqW = function (e) {
+					return y0 + (e.pageX - center.x) * settings.ratioV
+				}
 
-        }
+		}
 
-        else {
+		else {
 
-            if (settings.invertH)
-                var eqH = function (e) {
-                    return x0 - (e.pageX - center.x) * settings.ratioH
-                }
+			if (settings.invertH)
+				var eqH = function (e) {
+					return x0 - (e.pageX - center.x) * settings.ratioH
+				}
 
-            else
-                var eqH = function (e) {
-                    return x0 + (e.pageX - center.x) * settings.ratioH
-                }
+			else
+				var eqH = function (e) {
+					return x0 + (e.pageX - center.x) * settings.ratioH
+				}
 
-            if (settings.invertV)
-                var eqW = function (e) {
-                    return y0 - (e.pageY - center.y) * settings.ratioV
-                }
-            else
-                var eqW = function (e) {
-                    return y0 + (e.pageY - center.y) * settings.ratioV
-                }
+			if (settings.invertV)
+				var eqW = function (e) {
+					return y0 - (e.pageY - center.y) * settings.ratioV
+				}
+			else
+				var eqW = function (e) {
+					return y0 + (e.pageY - center.y) * settings.ratioV
+				}
 
-        }
+		}
 
-        $('html').on('mousemove', function (e) {
+		$('html').on('mousemove', function (e) {
 
-            x = eqH(e)
-            y = 0
+			x = eqH(e)
+			y = eqW(e)
 
-            $(layer).css({ top: y, left: x })
+			$(layer).css({ top: y, left: x })
 
-        })
+		})
 
-    };
+	};
 })(jQuery);
